db8353193ecd7f8d4809aacc15610f78f9368508
Lucascarbaj99 <58575523+Lucascarbaj99@users.noreply.github.com>
2020-02-14 15:17:45 -0300

    Add files via upload

--- MapsTest/Scripts/Asa.MapApi.js
+++ MapsTest/Scripts/Asa.MapApi.js
@@ -6,7 +6,7 @@ var mapApp = {
 mapApp.getPOIs = function (callback) {
     $.get("../api/pois", function (data) {
         console.log(data)
-        if (callback) callback(data.pois);
+        if (callback) callback(data.pos);
     });
 }
 
@@ -47,13 +47,49 @@ mapApp.initMap = function (callback) {
 mapApp.initMap(function () {
     mapApp.getPOIs(function (pois) {
         require(["esri/graphic", "esri/geometry/Point", "dojo/domReady!"],
-            function (Graphic, Point, GraphicLayer) {
+             function (Graphic, Point, GraphicLayer) {
                 pois.forEach(function (poiData) {
 
-                    var poi = new Point(poiData.XLon, poiData.YLat, mapApp.map.spatialReference)
+                    var poi = new Point(poiData.XLon, poiData.YLat, mapApp.POIsLayer.spatialReference)
                     mapApp.POIsLayer.add(new Graphic(poi, mapApp.POIsSymbol, poiData));
                 })
                 
             });
     });
 });
+
+
+function Validate() {
+    var latitude = $("#lat").val();
+    var longitude = $("#lng").val();
+
+    
+
+    //if (latitude < 180 && latitude > -180) {
+    //    //do nothing
+    //} else {
+    //    alert('Latitud erronea');
+    //}
+
+    if (longitude < 90) {
+        //do nothing
+    } else {
+        alert('longitud erronea');
+    }
+
+    function inrange(min, number, max) {
+        if (!isNaN(number) && (-number >= min) && (number <= max)) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+
+    if (inrange(-90, longitude, 90) && inrange(-180, longitude, 180)) {
+        $("#Guardar").removeAttr("disabled");
+        return true;
+    } else {
+        $("#Guardar").attr("disabled", "disabled");
+        return false;
+    }
+}
